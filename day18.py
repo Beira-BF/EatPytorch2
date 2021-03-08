97a1e43422501247dd32e9fca3ec23e9eac89934
Beira-BF <bf11846960696@qq.com>
2021-03-08 18:42:49 +0800

    day18, half day again

--- day18.py
+++ day18.py
@@ -20,3 +20,37 @@ from torchkeras import summary
 
 # 一，继承nn.Module基类构建自定义模型
 # 以下是继承nn.Module基类构建自定义模型的一个范例。模型中的用到的层一般在__init__函数中定义，然后在forward方法中定义模型的正向传播逻辑。
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.dropout = nn.Dropout2d(p=0.1)
+        self.adaptive_pool = nn.AdaptiveMaxPool2d((1,1))
+        self.flatten = nn.Flatten()
+        self.linear1 = nn.Linear(64,32)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(32,1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = self.dropout(x)
+        x = self.adaptive_pool(x)
+        x = self.flatten(x)
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+        y = self.sigmoid(x)
+        return y
+
+net = Net()
+print(net)
+
+summary(net, input_shape=(3,32,32))
+
