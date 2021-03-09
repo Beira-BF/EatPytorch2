ba2e78868c8ea37bc3621dbb141940750836c6ad
Beira-BF <bf11846960696@qq.com>
2021-03-09 18:52:07 +0800

    day 18, 构建模型的3中方法

--- day18.py
+++ day18.py
@@ -54,3 +54,158 @@ print(net)
 
 summary(net, input_shape=(3,32,32))
 
+# 二，使用nn.Sequential按层顺序构建模型
+# 使用nn.Sequential按层顺序构建模型无需定义forward方法，仅仅适合于简单的模型。
+# 以下是使用nn.Sequential搭建模型的一些等价方法。
+
+# 2，利用ann_module方法
+
+net = nn.Sequential()
+net.add_module("conv1", nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3))
+net.add_module("pool1", nn.MaxPool2d(kernel_size=2, stride=2))
+net.add_module("conv2", nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5))
+net.add_module("pool2", nn.MaxPool2d(kernel_size=2, stride=2))
+net.add_module("dropout", nn.Dropout2d(p=0.1))
+net.add_module("adaptive_pool", nn.AdaptiveMaxPool2d((1,1)))
+net.add_module("flatten", nn.Flatten())
+net.add_module("linear1", nn.Linear(64,32))
+net.add_module("relu", nn.ReLU())
+net.add_module("linear2", nn.Linear(32,1))
+net.add_module("sigmoid", nn.Sigmoid())
+
+print(net)
+
+# 2， 利用变长参数
+# 这种方式构建时不能给每个层指定名称。
+
+net = nn.Sequential(
+    nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3),
+    nn.MaxPool2d(kernel_size=2, stride=2),
+    nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5),
+    nn.MaxPool2d(kernel_size=2, stride=2),
+    nn.Dropout2d(p=0.1),
+    nn.AdaptiveMaxPool2d((1,1)),
+    nn.Flatten(),
+    nn.Linear(64,32),
+    nn.ReLU(),
+    nn.Linear(32,1),
+    nn.Sigmoid()
+)
+
+print(net)
+
+
+# 3, 利用OrderedDict
+from collections import OrderedDict
+
+net = nn.Sequential(OrderedDict(
+    [("conv1", nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)),
+     ("pool1", nn.MaxPool2d(kernel_size=2, stride=2)),
+     ("conv2", nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5)),
+     ("pool2", nn.MaxPool2d(kernel_size=2, stride=2)),
+     ("dropout", nn.Dropout2d(p=0.1)),
+     ("adaptive_pool", nn.AdaptiveMaxPool2d((1,1))),
+     ("flatten", nn.Linear(64,32)),
+     ("relu", nn.ReLU()),
+     ("linear2", nn.Linear(32,1)),
+     ("sigmoid", nn.Sigmoid())
+     ]
+))
+
+print(net)
+
+# summary(net, input_shape=(3,32,32))
+
+# 三、继承nn.Module基类构建模型并辅助应用模型容器进行封装
+# 当模型的结构比较复杂时，我们可以应用模型容器（nn.Sequential, nn.ModuleList, nn.ModuleDict)对模型的部分结构进行封装。
+# 这样做会让模型整体更加有层次感，有时候也能减少代码量。
+# 注意，在下面的范例中我们每次仅仅使用一种模型容器，但实际上这些模型容器的使用是非常灵活的，可以在一个模型中任意组合任意嵌套使用。
+
+# 1，nn.Sequential作为模型容器
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout2d(p=0.1),
+            nn.AdaptiveMaxPool2d((1,1))
+        )
+        self.dense = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64,32),
+            nn.ReLU(),
+            nn.Linear(32,1),
+            nn.Sigmoid()
+        )
+    def forward(self, x):
+        x = self.conv(x)
+        y = self.dense(x)
+        return y
+
+net = Net()
+print(net)
+
+# 2, nn.ModuleList作为模型容器
+# 注意下面中的ModuleList不能用Python中的列表代替。
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.layers = nn.ModuleList([
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout2d(p=0.1),
+            nn.AdaptiveMaxPool2d((1,1)),
+            nn.Flatten(),
+            nn.Linear(64,32),
+            nn.ReLU(),
+            nn.Linear(32,1),
+            nn.Sigmoid()]
+        )
+    def forward(self,x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+net = Net()
+print(net)
+
+# summary(net, input_shape=(3,32,32))
+
+# 3, nn.ModuleDict作为模型容器
+# 注意下面中的ModuleDict不能用Python中的字典代替。
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.layers_dict = nn.ModuleDict({"conv1":nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3),
+                                         "pool": nn.MaxPool2d(kernel_size=2, stride=2),
+                                         "conv2": nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5),
+                                         "dropout": nn.Dropout2d(p=0.1),
+                                         "adaptive": nn.AdaptiveMaxPool2d((1,1)),
+                                         "flatten": nn.Flatten(),
+                                         "linear1": nn.Linear(64,32),
+                                         "relu": nn.ReLU(),
+                                         "linear2": nn.Linear(32,1),
+                                         "sigmoid": nn.Sigmoid()
+                                         })
+
+    def forward(self, x):
+        layers = ["conv1", "pool", "conv2", "pool", "dropout", "adaptive",
+                  "flatten", "linear1", "relu", "linear2", "sigmoid"]
+        for layer in layers:
+            x = self.layers_dict[layer](x)
+        return x
+
+net = Net()
+print(net)
+
+summary(net, input_shape=(3,32,32))
+
